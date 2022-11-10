6f48448d85a51d14ffeb32d5d0414edc92c4ef15
u31504 <u31504@P31504>
2022-11-10 14:57:09 +1000

    Сделала задание 8.А

--- Window22.xaml.cs
+++ Window22.xaml.cs
@@ -23,5 +23,12 @@ namespace Wpf_Samkova_UP_PR4
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
     }
 }
